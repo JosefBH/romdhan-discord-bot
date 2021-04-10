e1a220b85f04266cd8043793bd8ce35c838d7266
JoesfBH <dlinkyoussef.123@gmail.com>
2021-04-10 17:03:16 +0100

    :bug: ahhh

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
-import os 
+import os
+import schedule 
+import time
+import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -12,59 +15,20 @@ intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix='?', description=description, intents=intents)
-@bot.command()
-async def on_asha(ctx):
-    while True:
-        now = datetime.now()
-        now = now.strftime('%Y-%m-%d %H:%M:00')
-        print(now)
-        if asha == now:
-            await ctx.send('@D3adshot#0587 ')    
 
+@bot.event
+async def lock(ctx):
+    await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False, read_messages=False)
 
 
 @bot.event
-async def on_ready():
-        print('Logged in as')
-        print(bot.user.name)
-        print(bot.user.id)
-        print('------')
+async def unlock(ctx):
+    await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True, read_messages=True)
 
-@bot.command()
-async def s(ctx):
-    print(ctx.guild.default_role) 
-@bot.command()
-async def add(ctx, left: float, right: float):
-    """Adds two numbers together."""""
-    await ctx.send(left + right)
+schedule.every().day.at('16:25').do()
 
-@bot.command()
-async def saymin(ctx):
-    await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False, read_messages=False)
-        
-
-@bot.command()
-async def ftarna(ctx):
-    await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True,read_messages=True)
-
+schedule.every().day.at('16:30').do(unlock())
 bot.run(token)
-'''
-@client.event
-async def isReady():
-    for guild in client.guilds: 
-        print(guild)
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('hello man !')
-        print(message.author, message.content)
-
-@client.command()
-async def perm(ctx):
-    print(ctx) 
-    await ctx.channel.set_permissions(send_messages=False)
-    
-client.run(token)
-'''
+while True:
+    schedule.run_pending()
+    time.sleep(1)
